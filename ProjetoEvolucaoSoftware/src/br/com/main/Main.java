39a602b98d8edae16f858fa64befef6036c6fce3
Thiago Krug <thiagockrug@gmail.com>
2013-02-28 00:29:15 -0300

    first commit :D

---
+++ ProjetoEvolucaoSoftware/src/br/com/main/Main.java
@@ -0,0 +1,17 @@
+package br.com.main;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author thiago
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        // Criar main aqui
+        System.out.println("Hello World!");
+        JOptionPane.showMessageDialog(null, "Hello World!");
+    }
+    
+}
