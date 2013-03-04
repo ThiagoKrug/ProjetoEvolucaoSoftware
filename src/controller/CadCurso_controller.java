725d93cee2df306bf5b326d85762f43442b42676
douglas almeida <gattuso@92.242.140.20>
2013-03-04 17:28:45 -0300

    Construtor cria tela no sistema

--- src/controller/CadCurso_controller.java
+++ src/controller/CadCurso_controller.java
@@ -1,12 +1,22 @@
 
 package controller;
 
+import util.Funcoes;
+import view.janCadCurso;
+
 /**
  *
  * @author Douglas F. Almeida- 101151405
  * @version 28/02/2013
  * 
  */
-public class CadCurso_controller {
+public class CadCurso_controller extends Funcoes {
     
+  public CadCurso_controller(){
+      
+    janCadCurso nCurso = new janCadCurso();
+    nCurso.setVisible(true);
+    centralizar(nCurso);
+    
+  }
 }
