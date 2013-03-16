d12c9f35d19fc6b43f400beb91c9c49a262903e3
Marcelo Maia Lopes <maialopesx@gmail.com>
2013-03-16 20:19:12 -0300

    Criação do DAO de ProvaMemorial, está faltando apenas o teste Unitário.

--- src/br/com/model/dao/ProvaMemorialDao.java
+++ src/br/com/model/dao/ProvaMemorialDao.java
@@ -4,8 +4,16 @@
  */
 package br.com.model.dao;
 
+import br.com.jdbc.ConnectionFactory;
 import br.com.model.entity.IEntidade;
+import br.com.model.entity.ProvaMemorial;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,34 +23,160 @@ import java.util.List;
 public class ProvaMemorialDao  implements IDao{
 
     @Override
-    public IEntidade inserir(IEntidade entidade) {
-       
+    public IEntidade inserir(IEntidade entidade) throws SQLException {
+      
+        if(entidade instanceof ProvaMemorial){
+          
+          ProvaMemorial provaMemorial = (ProvaMemorial) entidade;
+          
+          String sql = "insert into prova_memorial (id_concurso,data_comparecimento,compareceu) values(?,?,?) ";
+         
+            Connection connection = ConnectionFactory.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            stmt.setInt(1, provaMemorial.getConcurso().getIdConcurso());
+            stmt.setDate(2, new Date (provaMemorial.getData_comparecimento().getTime()));
+            stmt.setBoolean(3, provaMemorial.getCompareceu());
+            stmt.executeUpdate();
+            
+            ResultSet rs = stmt.getGeneratedKeys();
+            
+            if (rs.next()) {
+                
+                provaMemorial.setIdProvaMemorial(rs.getInt(1));
+                
+            }
+            
+            stmt.close();
+            return (IEntidade) provaMemorial;
+      } 
       return null;
     }
 
     @Override
-    public IEntidade alterar(IEntidade entidade) {
+    public IEntidade alterar(IEntidade entidade) throws SQLException {
+        if(entidade instanceof ProvaMemorial){
+          
+          ProvaMemorial provaMemorial = (ProvaMemorial) entidade;
+          String sql = "update prova_memorial set id_concurso=?,data_comparecimento=?,compareceu=? where id_prova_memorial=? ";
+         
+            Connection connection = ConnectionFactory.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            stmt.setInt(1, provaMemorial.getConcurso().getIdConcurso());
+            stmt.setDate(2, new Date (provaMemorial.getData_comparecimento().getTime()));
+            stmt.setBoolean(3, provaMemorial.getCompareceu());
+            stmt.executeUpdate();
+            
+            ResultSet rs = stmt.getGeneratedKeys();
+            
+            if (rs.next()) {
+                
+                provaMemorial.setIdProvaMemorial(rs.getInt(1));
+                
+            }
+            
+            stmt.close();
+            return (IEntidade) provaMemorial;
+      }
         return null;
     }
 
     @Override
     public IEntidade excluir(IEntidade entidade) {
+        if(entidade instanceof ProvaMemorial){
+           String sql = "delete from prova_memorial where id_prova_memorial=?";
+            ProvaMemorial provaMemorial = (ProvaMemorial) entidade;
+        try {
+            Connection connection = ConnectionFactory.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1,provaMemorial.getIdProvaMemorial());
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+        }
         return null;
     }
 
     @Override
     public IEntidade pesquisarPorId(int id) {
-       return null;
+       ProvaMemorial provaMemorial = new ProvaMemorial();
+       String sql = "select * from prova_memorial where id_prova_memorial = ?"; 
+       try {
+            Connection connection = ConnectionFactory.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            int posicao = 0;
+            while (rs.next()) {
+                provaMemorial.setIdProvaMemorial(rs.getInt("id_prova_memorial"));
+                provaMemorial.setCompareceu(rs.getBoolean("compareceu"));
+                provaMemorial.setData_comparecimento(rs.getDate("data_comparecimento"));
+                provaMemorial.setConcurso(null);
+                
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+       return (IEntidade) provaMemorial;
     }
 
     @Override
     public List<IEntidade> pesquisarTodos()  {
-        return null;
+       List<IEntidade> listaProvaMemorial = new ArrayList<IEntidade>() ;
+      
+       String sql = "select * from prova_memorial"; 
+       try {
+            Connection connection = ConnectionFactory.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            int posicao = 0;
+            while (rs.next()) {
+                ProvaMemorial provaMemorial = new ProvaMemorial();
+                provaMemorial.setIdProvaMemorial(rs.getInt("id_prova_memorial"));
+                provaMemorial.setCompareceu(rs.getBoolean("compareceu"));
+                provaMemorial.setData_comparecimento(rs.getDate("data_comparecimento"));
+                provaMemorial.setConcurso(null);
+                
+                listaProvaMemorial.add(provaMemorial);
+                
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+       return  listaProvaMemorial;
     }
 
     @Override
     public List<IEntidade> pesquisarTodosOrdenadoPor(String criterioOrdenamento) {
-     return null;
+     List<IEntidade> listaProvaMemorial = new ArrayList<IEntidade>();
+      
+       String sql = "select * from prova_memorial order by "+criterioOrdenamento; 
+       try {
+            Connection connection = ConnectionFactory.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            int posicao = 0;
+            while (rs.next()) {
+                ProvaMemorial provaMemorial = new ProvaMemorial();
+                provaMemorial.setIdProvaMemorial(rs.getInt("id_prova_memorial"));
+                provaMemorial.setCompareceu(rs.getBoolean("compareceu"));
+                provaMemorial.setData_comparecimento(rs.getDate("data_comparecimento"));
+                provaMemorial.setConcurso(null);
+                
+                listaProvaMemorial.add(provaMemorial);
+                
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+       return  listaProvaMemorial;
     }
     
 }
--- src/br/com/model/entity/ProvaMemorial.java
+++ src/br/com/model/entity/ProvaMemorial.java
@@ -5,13 +5,14 @@
 package br.com.model.entity;
 
 import java.util.ArrayList;
+import java.util.Date;
 import javax.validation.constraints.NotNull;
 
 /**
  *
  * @author Bruno
  */
-public class ProvaMemorial {
+public class ProvaMemorial implements IEntidade{
    
    @NotNull 
    private int idProvaMemorial;
@@ -19,6 +20,30 @@ public class ProvaMemorial {
    private Concurso concurso;
    @NotNull 
    private ArrayList<CriterioAvaliacaoProvaMemorial> criterios;
+   @NotNull
+   private boolean compareceu;
+   @NotNull
+   private Date data_comparecimento;
+
+    public Date getData_comparecimento() {
+        return data_comparecimento;
+    }
+
+    public void setData_comparecimento(Date data_comparecimento) {
+        this.data_comparecimento = data_comparecimento;
+    }
+   
+   
+
+    public boolean getCompareceu() {
+        return compareceu;
+    }
+
+    public void setCompareceu(boolean compareceu) {
+        this.compareceu = compareceu;
+    }
+   
+   
 
     public int getIdProvaMemorial() {
         return idProvaMemorial;
