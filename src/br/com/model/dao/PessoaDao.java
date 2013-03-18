58f5d3bc815be2971eb706ebb196148226274871
Helison Reus Teixeira <helisonreus@gmail.com>
2013-03-18 16:42:44 -0300

    Classe PessoaDao criada

---
+++ src/br/com/model/dao/PessoaDao.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.model.dao;
+
+import br.com.model.entity.IEntidade;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author Helison
+ */
+public class PessoaDao implements IDao{
+
+    @Override
+    public IEntidade inserir(IEntidade entidade) throws SQLException {
+        
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public IEntidade alterar(IEntidade entidade) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public IEntidade excluir(IEntidade entidade) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public IEntidade pesquisarPorId(int id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<? extends IEntidade> pesquisarTodos() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<? extends IEntidade> pesquisarTodosOrdenadoPor(String criterioOrdenamento) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
