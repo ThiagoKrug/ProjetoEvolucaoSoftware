4715773bf76ade27323a87ff0e143616d1d6fc9b
Usuario <Usuario@10.2.8.21>
2013-03-21 20:37:26 -0300

    Alterações

--- src/br/com/model/entity/Titulacao.java
+++ src/br/com/model/entity/Titulacao.java
@@ -4,6 +4,8 @@
  */
 package br.com.model.entity;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.NotBlank;
 
@@ -12,9 +14,10 @@ import org.hibernate.validator.constraints.NotBlank;
  * @author Bruno
  */
 public class Titulacao implements IEntidade {
+    private HashMap<String, Method[]> tablemap;
     
     @NotNull 
-    private int idTitulacao;
+    private Integer idTitulacao;
     @NotBlank
     private String titulacao;
 
@@ -23,6 +26,24 @@ public class Titulacao implements IEntidade {
     }
 
     public Titulacao() {
+        this.tablemap = new HashMap<String, Method[]>();
+        try {
+        Method[] ids = new Method[] {
+            this.getClass().getMethod("getIdTitulacao", new Class<?>[] {}),
+            this.getClass().getMethod("setIdTitulacao", new Class<?>[] {this.idTitulacao.getClass()})
+        };
+        this.tablemap.put("id_titulacao", ids);
+        
+        this.tablemap.put("titulacao", new Method[] {
+            this.getClass().getMethod("getTitulacao", new Class<?>[] {}),
+            this.getClass().getMethod("setTitulacao", new Class<?>[] {this.titulacao.getClass()})
+        });
+
+        
+        } catch (NoSuchMethodException e) {
+            System.out.println("Erro na reflection.");
+            e.printStackTrace();
+        }
     }
 
     public int getIdTitulacao() {
@@ -39,6 +60,20 @@ public class Titulacao implements IEntidade {
 
     public void setTitulacao(String titulacao) {
         this.titulacao = titulacao;
+    }
+
+    /**
+     * @return the tablemap
+     */
+    public HashMap<String, Method[]> getTablemap() {
+        return tablemap;
+    }
+
+    /**
+     * @param tablemap the tablemap to set
+     */
+    public void setTablemap(HashMap<String, Method[]> tablemap) {
+        this.tablemap = tablemap;
     }
     
     
