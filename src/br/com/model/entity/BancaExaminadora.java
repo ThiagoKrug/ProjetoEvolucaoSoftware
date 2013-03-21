d6b377e5a48507c8e1c1cc7a144d2000afb24280
Thiago Lima <thiagolimaes@gmail.com>
2013-03-21 20:40:01 -0300

    Criada classe BancaExaminadora.

--- src/br/com/model/entity/BancaExaminadora.java
+++ src/br/com/model/entity/BancaExaminadora.java
@@ -1,15 +1,71 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.com.model.entity;
 
+import br.com.model.dao.ConcursoDao;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
- *
- * @author Bruno
+ * @author Thiago Lima <thiagolimaes@gmail.com>
  */
 public class BancaExaminadora implements IEntidade {
     
     private int idBanca;
+    private int idConcurso;
+    private ArrayList<Examinador> examinadores;
+    
+    public BancaExaminadora() {
+        
+    }
+    
+    /**
+     * Adiciona examinadores na Banca.
+     * @param examinador 
+     */
+    public void adicionaExaminador(Examinador examinador) {
+        
+        if (this.examinadores.size() < 2) {
+            this.examinadores.add(examinador);
+        }
+        
+    }
+    
+    /**
+     * Retorna o objeto do Concurso da banca examinadora.
+     * @return Concurso
+     * @throws SQLException 
+     */
+    public Concurso getConcurso() throws SQLException {
+        
+        ConcursoDao concursoDao = new ConcursoDao();
+        Concurso concurso       = (Concurso) concursoDao.pesquisarPorId(this.idConcurso);
+        
+        return concurso;
+        
+    }
+
+    public int getIdBanca() {
+        return idBanca;
+    }
+
+    public int getIdConcurso() {
+        return idConcurso;
+    }
+
+    public ArrayList<Examinador> getExaminadores() {
+        return examinadores;
+    }
+
+    public void setIdBanca(int idBanca) {
+        this.idBanca = idBanca;
+    }
+
+    public void setIdConcurso(int idConcurso) {
+        this.idConcurso = idConcurso;
+    }
+
+    public void setExaminadores(ArrayList<Examinador> examinadores) {
+        this.examinadores = examinadores;
+    }
     
 }
