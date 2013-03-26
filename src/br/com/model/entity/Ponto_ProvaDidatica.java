8f2696361f3bf80d5b0c320b0a58ac6b7f305dde
Juliano <j.rodovalho.m@gmail.com>
2013-03-26 07:31:34 -0300

    [Ponto_ProvaDidatica.java] Construção da arquitetura da Classe.

--- src/br/com/model/entity/Ponto_ProvaDidatica.java
+++ src/br/com/model/entity/Ponto_ProvaDidatica.java
@@ -8,6 +8,41 @@ package br.com.model.entity;
  *
  * @author Juliano R. M.
  */
-public class Ponto_ProvaDidatica {
+public class Ponto_ProvaDidatica implements IEntidade {
+
+    private int idPontoProvaDidatica;
+    private String descricaoPonto;
+    private ProvaDidatica provaDidatica;
+
+    public Ponto_ProvaDidatica(int idPontoProvaDidatica, String descricaoPonto) {
+        this.idPontoProvaDidatica = idPontoProvaDidatica;
+        this.descricaoPonto = descricaoPonto;
+    }
+
+    public int getIdPontoProvaDidatica() {
+        return idPontoProvaDidatica;
+    }
+
+    public void setIdPontoProvaDidatica(int idPontoProvaDidatica) {
+        this.idPontoProvaDidatica = idPontoProvaDidatica;
+    }
+
+    public String getDescricaoPonto() {
+        return descricaoPonto;
+    }
+
+    public void setDescricaoPonto(String descricaoPonto) {
+        this.descricaoPonto = descricaoPonto;
+    }
+
+    public ProvaDidatica getProvaDidatica() {
+        return provaDidatica;
+    }
+
+    public void setProvaDidatica(ProvaDidatica provaDidatica) {
+        this.provaDidatica = provaDidatica;
+    }
+    
+    
     
 }
