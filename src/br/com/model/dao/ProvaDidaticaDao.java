b5a5d09fdaec106036e280a9471f7a6485e246ea
Juliano <j.rodovalho.m@gmail.com>
2013-03-27 01:31:24 -0300

    [ProvaDidaticaDao] Inicio da construção dos métodos de alteração e exclusão da Prova Didática.

--- src/br/com/model/dao/ProvaDidaticaDao.java
+++ src/br/com/model/dao/ProvaDidaticaDao.java
@@ -5,6 +5,7 @@
 package br.com.model.dao;
 
 import br.com.jdbc.ConnectionFactory;
+import br.com.model.entity.Candidato;
 import br.com.model.entity.Concurso;
 import br.com.model.entity.IEntidade;
 import br.com.model.entity.ProvaDidatica;
@@ -15,6 +16,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.AbstractList;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -24,7 +26,7 @@ import java.util.List;
 public class ProvaDidaticaDao implements IDao {
 
     @Override
-    public IEntidade inserir(IEntidade entidade) throws SQLException {
+    public ProvaDidatica inserir(IEntidade entidade) throws SQLException {
         if (entidade instanceof ProvaDidatica) {
 
             ProvaDidatica provaDidatica = (ProvaDidatica) entidade;
@@ -60,14 +62,93 @@ public class ProvaDidaticaDao implements IDao {
         return null;
     }
 
+    /**
+     * Inda em CONSTRUÇÃO!
+     * @param entidade
+     * @return
+     * @throws SQLException 
+     */
     @Override
-    public IEntidade alterar(IEntidade entidade) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ProvaDidatica alterar(IEntidade entidade) throws SQLException {
+
+        Connection connection = ConnectionFactory.getConnection();
+        ProvaDidatica provaDidatica = (ProvaDidatica) entidade;
+
+        String sql_Altera = "DELETE FROM ";
+        PreparedStatement stmtAletrar = connection.prepareStatement(sql_Altera);
+        stmtAletrar.setInt(1, provaDidatica.getIdProvaDidatica());
+        stmtAletrar.executeUpdate();
+
+        if (!provaDidatica.getCandidatosAptosProvaDidatica().isEmpty()) {
+
+            ArrayList<Candidato> listaAptos = provaDidatica.getCandidatosAptosProvaDidatica();
+            Iterator<Candidato> iterator = listaAptos.iterator();
+
+            while (iterator.hasNext()) {
+
+                Candidato objCandidato = iterator.next();
+                String sql_InsereApatos = "INSERT INTO ";
+                PreparedStatement statement = connection.prepareStatement(sql_InsereApatos, Statement.RETURN_GENERATED_KEYS);
+                statement.setInt(1, objCandidato.getIdCandidato());
+                statement.setInt(1, provaDidatica.getIdProvaDidatica());
+                statement.executeUpdate();
+            }
+        }
+
+        String sql_DeletaAptos = "";
+        PreparedStatement statement2 = connection.prepareStatement(sql_DeletaAptos);
+        statement2.setInt(1, provaDidatica.getIdProvaDidatica());
+        statement2.executeUpdate();
+
+        if (!provaDidatica.getCandidatosAptosProvaDidatica().isEmpty()) {
+
+            ArrayList<Candidato> listaAptos = provaDidatica.getCandidatosAptosProvaDidatica();
+            Iterator<Candidato> iterator = listaAptos.iterator();
+            /*remove todos os candidadtos aptos*/
+
+            while (iterator.hasNext()) {
+
+                Candidato obj_Candidato = iterator.next();
+                String sql_Insere = "INSERT INTO ";
+
+                PreparedStatement stmt5 = connection.prepareStatement(sql_Insere, Statement.RETURN_GENERATED_KEYS);
+                stmt5.setInt(1, obj_Candidato.getIdCandidato());
+                stmt5.setInt(1, provaDidatica.getIdProvaDidatica());
+                stmt5.executeUpdate();
+            }
+        }
+
+        String sql_Update = "UPDATE ";
+
+        PreparedStatement stmt6 = connection.prepareStatement(sql_Update);
+//        this.setInt(stmt6, 1, value);
+//        this.setInt(stmt6, 2, value);
+//        this.setInt(stmt6, 3, value);
+
+        System.out.println(stmt6.toString());
+        if (stmt6.executeUpdate() == 1) {
+
+            return provaDidatica;
+        }
+        return null;
     }
 
     @Override
-    public IEntidade excluir(IEntidade entidade) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ProvaDidatica excluir(IEntidade entidade) throws SQLException {
+        if (entidade instanceof ProvaDidatica) {
+
+            ProvaDidatica provaDidatica = (ProvaDidatica) entidade;
+            String sql_Excluir = "DELETE FROM prova_didatica WHERE id_prova_didatica = ?";
+
+            Connection connection = ConnectionFactory.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql_Excluir);
+            stmt.setInt(1, provaDidatica.getIdProvaDidatica());
+
+            if (stmt.executeUpdate() == 1) {
+                return provaDidatica;
+            }
+        }
+        return null;
     }
 
     @Override
